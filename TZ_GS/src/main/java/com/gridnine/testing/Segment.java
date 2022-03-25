694e34fb8615aab7c4d903b6096be7c8c7d410a7
Malyush-consolidate <andrey.m.30@mail.ru>
2022-03-25 12:38:21 +0300

    Initial commit

---
+++ TZ_GS/src/main/java/com/gridnine/testing/Segment.java
@@ -0,0 +1,37 @@
+package com.gridnine.testing;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Segment {
+    private final Integer id;
+    private final LocalDateTime departureDate;
+    private final LocalDateTime arrivalDate;
+
+    Segment(final LocalDateTime departure, final LocalDateTime arrival, Integer id) {
+        departureDate = Objects.requireNonNull(departure);
+        arrivalDate = Objects.requireNonNull(arrival);
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    LocalDateTime getDepartureDate() {
+        return departureDate;
+    }
+
+    LocalDateTime getArrivalDate() {
+        return arrivalDate;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd' T 'HH:mm");
+        return '(' + departureDate.format(formatter) + '|' + arrivalDate.format(formatter)
+                + ')';
+    }
+}
